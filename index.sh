3a948db57d90d435c16ab4db850f71c0639087b4
Steven Lu (5950X 20.04[tmux]) <stevenlu443@gmail.com>
2021-12-09 02:29:49 -0500

    imrpoved

--- index.sh
+++ index.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# helper
+COLOR=$'\x1b[35m'
+COLORSTRONG=$'\x1b[31m'
+RESET=$'\x1b[m'
+
+# helpers
 execute () {
   CTR=1
-  printf "Executing the following:"
+  printf "$COLOR$(date)$RESET: "
   for arg in "$@"; do
     printf "\x1b[30m"'$'"%s=\x1b[33m%s\x1b[m " $CTR "$arg"
     (( CTR ++ ))
@@ -52,17 +56,13 @@ mkdir -p "$REPO_METADATA_PATH"
 touch "$REPO_METADATA_PATH/commit_reported_cache"
 mkdir -p "$REPO_RESULTS_PATH"
 
-COLOR=$'\x1b[35m'
-COLORSTRONG=$'\x1b[31m'
-RESET=$'\x1b[m'
-
 while true; do
   # Periodic Processing
   # - only run logic on newly seen pairs of active commits
 
   cd "$REPO_PATH" || exit 2
 
-  GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" execute git fetch
+  GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" git fetch
 
   {
     git for-each-ref --sort=committerdate refs/remotes/ --format='%(committerdate:short) %(objectname:short) %(refname:short)'
@@ -81,12 +81,12 @@ print(*lines, sep="\n")' | while read -r A_HASH A B_HASH B; do
         MERGEOUTPUT="$(git merge --no-commit --no-ff "$B_HASH" 2>&1)"
         MERGEABLE=$?
         echo "$COLOR$(date)$RESET $COMMIT_PAIR ##### >>>$MERGEOUTPUT<<<" >> "$REPO_RESULTS_PATH/output"
-        if [ ! "$MERGEABLE" = 0 ]; then echo "${COLORSTRONG}FAILED TO MERGE $COMMIT_PAIR"; fi
+        if [ ! "$MERGEABLE" = 0 ]; then echo "$COLOR$(date) ${COLORSTRONG}FAILED TO MERGE $COMMIT_PAIR"; fi
         echo "$COLOR$(date)$RESET $COMMIT_PAIR retcode >>>$MERGEABLE<<<" >> "$REPO_RESULTS_PATH/mergeable"
         echo "$COLOR$(date)$RESET $COMMIT_PAIR ===== >>>$(git diff --cached)<<<" >> "$REPO_RESULTS_PATH/diffcached"
         echo "$COLOR$(date)$RESET $COMMIT_PAIR ----- >>>$(git diff)<<<" >> "$REPO_RESULTS_PATH/diff"
-        echo "$COLOR$(date)$RESET $COMMIT_PAIR =stat= >>>$(git diff --cached --stat)<<<"
-        echo "$COLOR$(date)$RESET $COMMIT_PAIR -stat- >>>$(git diff --stat)<<<"
+        execute git diff --cached --stat
+        execute git diff --stat
         echo "$COMMIT_PAIR" >> "$REPO_METADATA_PATH/commit_reported_cache"
         if [ ! "$MERGEABLE" = "0" ]; then
           execute git merge --abort
